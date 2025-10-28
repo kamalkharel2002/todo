47be56e5bdf960b5ecaecf6fa6ec8575bd060634
kamal <kamalkharel2002@gmail.com>
2025-10-28 23:33:28 +0600

    Initial commit

--- start-backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting Todo App Backend..."
-echo "Make sure MySQL and Redis are running!"
-echo "Backend will run on http://localhost:5001"
-echo ""
-
-cd Backend
-npm start
--- start-frontend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting Todo App Frontend..."
-echo "Frontend will run on http://localhost:3000"
-echo ""
-
-cd Frontend/todo-frontend
-npm start
